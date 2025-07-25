e290208c10aa487a3e021d046946815e3d602e5f
ac-devs19 <ac.devs.07192002@gmail.com>
2025-07-25 09:25:26 +0800

    remove function in api route

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,4 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/mobile/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required',
-        'password' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        return response()->json(['message' => 'Invalid credentials'], 401);
-    }
-
-    $token = $user->createToken('mobile-token')->plainTextToken;
-
-    return response()->json([
-        'token' => $token,
-        'user' => $user,
-    ]);
-});
