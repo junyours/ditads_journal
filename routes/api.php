d570c1b45cf32b683c531b8cc0e78e777efacf2b
ac-devs19 <ac.devs.07192002@gmail.com>
2025-07-26 13:04:23 +0800

    add function in api route

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,32 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/mobile/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'The provided credentials are incorrect.'], 422);
+    }
+
+    $token = $user->createToken('mobile-token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+    ]);
+});
+
+Route::get('/mobile/logout', function (Request $request) {
+    $user = $request->user();
+
+    $user->currentAccessToken()->delete();
+
+    return response()->noContent();
+})->middleware('auth:sanctum');
