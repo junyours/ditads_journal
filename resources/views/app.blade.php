981a1cc60b8c2de7e22b78fab060acfd4a226597
ac-devs19 <ac.devs.07192002@gmail.com>
2025-08-19 15:36:47 +0800

    add meta google site verification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="R_0o7TCe9W3RXtB8e3fkpcH7GebAtjpzwDacgcG0tpU" />
         <link rel="shortcut icon" href="/images/ditads-logo.png" type="image/x-icon" />
 
         <title inertia>{{ config('app.name') }}</title>
