75930144b5788128f8d88234314624eda1195d6a
ac-devs19 <ac.devs.07192002@gmail.com>
2025-08-28 11:34:43 +0800

    add memory limit

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+ini_set('memory_limit', -1);
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
