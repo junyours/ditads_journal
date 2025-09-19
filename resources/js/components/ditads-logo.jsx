8ed60a9c7e9ff754c29746a3af059676ce480f6e
ac-devs19 <ac.devs.07192002@gmail.com>
2025-09-19 09:46:16 +0800

    change banner

--- resources/js/components/ditads-logo.jsx
+++ resources/js/components/ditads-logo.jsx
@@ -1,5 +1,11 @@
 import Logo from "../../../public/images/ditads-logo.png";
 
 export default function DitadsLogo() {
-    return <img src={Logo} className="object-contain" alt="ditads-logo" />;
+    return (
+        <img
+            src={Logo}
+            className="object-contain rounded-full"
+            alt="ditads-logo"
+        />
+    );
 }
